2bd0c728937c010a0a7057ae9ea2deee0a623017
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2026-01-28 22:22:35 +0100

    update api

--- clients/python/api.py
+++ clients/python/api.py
@@ -1,87 +1,113 @@
 
 from abc import ABC
 from dataclasses import dataclass
-from typing import NewType
-from uuid import UUID
+from io import StringIO
+from typing import AsyncGenerator, TypeAlias, Sequence, Literal
+from typing import TypeAlias
+# All ids are immutable and generated at the ressource creation (based on the name provided and a random value to ensure unicity)
 
-
-DatasetId = NewType('DatasetId', str)
-EnvId = NewType('EnvId', str)
-PipelineId = NewType('PipelineId', str)
-AppId = NewType('AppId', str)
-ExperimentId = NewType('ExperimentId', str)
-
+# --- App properties, stored in yaml file ---
 
 @dataclass
 class Dataset:
-    id: DatasetId
+    id: str
     name: str
 
 @dataclass
 class Env:
-    id: EnvId
+    id: str
     url: str
+    name: str
 
 @dataclass
 class Pipeline:
-    id: PipelineId
+    id: str
+    name: str
     route: str
-    dataset_id: DatasetId
+    dataset_id: str
 
 
 @dataclass
 class AppHeader:
-    id: AppId
+    id: str
     name: str
 
 # One yaml file by App, ({app_id}.yaml)
 @dataclass
 class App:
-    id: AppId
-    version: UUID # to prevent concurrent updates or Experiment started on an deprecated version of the App.
+    id: str
+    version: int # to prevent concurrent updates or Experiment started on an deprecated version of the App.
     name: str
     envs: list[Env]
+    datasets: list[Dataset]
     pipelines: list[Pipeline]
+
+
+# --- Experiments and Dataset content, stored in lancedb ----
+
+@dataclass
+class ExperimentConfig:
+    app_id: str
+    app_version: int
+    pipeline_id: str
+    name: str
 
 @dataclass
 class Experiment:
-    app_id: AppId
-    app_version: UUID
-    pipeline_id: PipelineId
+    id: str
+    config: ExperimentConfig
+    status: Literal["not_started","running","completed","stopped"]
 
+JSONValue: TypeAlias = (
+    dict[str, "JSONValue"] 
+    | list["JSONValue"] 
+    | str 
+    | int 
+    | float 
+    | bool 
+    | None
+)
 
-
-
-
-@dataclass
-class ExperimentResult:
-    result: StreamingResponse | PaginatedResponse # Depending on weither the expermient is ongoing or finished
-
+class ExperimentSampleResult:
+    sample_id: str
+    result: JSONValue
 
 class Evalessence(ABC):
 
-    async def get_app_list(self) -> list[AppHeader]:
+    # ---- App -----
+    async def list_apps(self) -> list[AppHeader]:
         raise NotImplementedError()
 
-    async def create_app(self, name: str) -> AppId:
+    async def create_app(self, name: str) -> App:
         raise NotImplementedError()
 
-    async def get_app(self, app_id: AppId) -> App:
+    async def get_app(self, app_id: str) -> App:
         raise NotImplementedError()
     
-    async def delete_app(self, app_id: AppId, app_version: UUID) -> None:
+    async def delete_app(self, app_id: str, app_version: int) -> None:
         raise NotImplementedError()
 
-    async def update_app(self, app: App, app_version: UUID) -> App:
+    async def update_app(self, app: App) -> App:
         raise NotImplementedError()
 
-    async def upload_dataset()# TODO manage dataset CRUD
-    # Todo: do i need dataset versioning ?
-    # should i do pipelin versionning ?
+    # --- Datasets and experiments ---
 
 
-    async def run_experiment(self, experiment: Experiment) -> ExperimentId:
+    async def upload_file_to_dataset(self, app_id: str, dataset_id: str, file: StringIO) -> None:
+        "file must be a jsonl file, where each entry has a unique 'sample_id' key"
         raise NotImplementedError()
 
-    def get_experiment(self, experiment_id: ExperimentId) -> ExperimentResult:
+    async def run_experiment(self, experiment: ExperimentConfig) -> Experiment:
+        raise NotImplementedError()
+    
+    async def list_experiments(self, app_id: str, pipeline_id: str) -> list[Experiment]:
+        raise NotImplementedError()
+    
+    async def get_experiment(self, app_id: str, experiment_id: str) -> Experiment:
+        raise NotImplementedError()
+
+    async def stream_experiment_results(self, experiment_id: str) -> AsyncGenerator[ExperimentSampleResult, None]:
+        raise NotImplementedError()
+    
+    async def load_experiment_results(self, experiment_id: str) -> Sequence[ExperimentSampleResult]:
         raise NotImplementedError()
