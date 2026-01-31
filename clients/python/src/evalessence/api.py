a0635db31e966ebd963b8e7d9a5ea33ea9a025a8
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2026-01-31 16:57:37 +0100

    add proper python client setup

--- clients/python/src/evalessence/api.py
+++ clients/python/src/evalessence/api.py
@@ -2,8 +2,10 @@
 from abc import ABC
 from dataclasses import dataclass
 from io import StringIO
-from typing import AsyncGenerator, TypeAlias, Sequence, Literal
+from typing import AsyncGenerator, TypeAlias, Sequence, Literal, Any
 from typing import TypeAlias
+import pyarrow as pa 
+
 # All ids are immutable and generated at the ressource creation (based on the name provided and a random value to ensure unicity)
 
 # --- App properties, stored in yaml file ---
@@ -68,6 +70,10 @@ JSONValue: TypeAlias = (
     | None
 )
 
+class Sample:
+    sample_id: str
+    value: JSONValue
+
 class ExperimentSampleResult:
     sample_id: str
     result: JSONValue
@@ -90,18 +96,49 @@ class Evalessence(ABC):
     async def get_app(self, app_id: str) -> App:
         raise NotImplementedError()
     
-    async def delete_app(self, app_id: str, app_version: int) -> None:
+    async def delete_app(self, app_id: str) -> None:
         raise NotImplementedError()
 
     async def update_app(self, app: App) -> App:
         raise NotImplementedError()
 
-    # --- Datasets and experiments ---
+    # --- Datasets ---
+
+    async def update_dataset(self, app_id: str, dataset_id: str, upsert_by_id: pa.RecordBatchReader[Sample], delete_by_id: pa.Array[str]) -> list[str]:
+        # atomic, return the IDs of the added samples.
+        raise NotImplementedError()
+
+    async def select(self, app_id: str, dataset_id: str, filter_expr: str | None, sort_by: str = "id", descending: bool = False, after_elt: tuple[Any, str] | None = None, limit: int | None = None) -> pa.RecordBatchReader[Sample]:
+        """
+        Docstring for select
+        
+        :param self: Description
+        :param app_id: Description
+        :type app_id: str
+        :param dataset_id: Description
+        :type dataset_id: str
+        :param filter_expr: Description
+        :type filter_expr: str | None
+        :param sort_by: Description
+        :type sort_by: str
+        :param after_elt: first tuple entry is consistant value with "sort_by" column, second tuple item is the id column.
+        :type after_elt: Any | None
+        :param limit: Description
+        :type limit: int | None
+        :return: Description
+        :rtype: Any
+        """
+        raise NotImplementedError()
+
+
 
     # TODO: at the level of the Code API, it should probably be a list ?
     async def upload_file_to_dataset(self, app_id: str, dataset_id: str, file: StringIO) -> None:
         "file must be a jsonl file, where each entry has a unique 'sample_id' key"
         raise NotImplementedError()
+
+
+    # ----- Experiments -----------
 
     async def run_experiment(self, experiment: ExperimentConfig) -> Experiment:
         raise NotImplementedError()
